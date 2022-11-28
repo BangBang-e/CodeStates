08e74f20510d2f7c899086d2844b796955e1f2ff
YOUNG-JAE KIM <107192580+BangBang-e@users.noreply.github.com>
2022-11-28 23:52:40 +0900

    코드 스테이츠 fe-Section 2 [Unit 5]
    fe-sprint-react-twittler-spa 과제

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 // TODO - import문을 이용하여 react-router-dom 라이브러리의 Link 컴포넌트를 불러옵니다.
+import { useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    navigate(-1)
+  }
+  const handleGoForward = () => {
+    navigate(1)
+  }
+  const handleReload = () => {
+    navigate(0)
+  }
+  
   return (
     <section className="sidebar">
       {/* TODO : About 메뉴 아이콘과 Mypage 메뉴 아이콘을 작성하고 Link 컴포넌트를 이용하여 경로(path)를 연결합니다. */}
-        <i className="far fa-comment-dots"></i>
+        <Link onClick={handleGoBack}><span className="far">◀︎</span></Link>
+        <Link onClick={handleGoForward}><span className="far">▶︎</span></Link>
+        <Link onClick={handleReload}><span className="far">reset</span></Link>
+        <Link to="/"><i className="far fa-comment-dots"></i></Link>
+        <Link to="/about"><i className="far fa-question-circle"></i></Link>
+        <Link to="/mypage"><i className="far fa-user"></i></Link>
     </section>
   );
 };
 
 export default Sidebar;
+
+{/* <Link onClick={handleGoBack}><i className="far fa-circle-left"></i></Link> */}
