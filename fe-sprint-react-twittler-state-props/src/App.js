979f33253d2399c04e05625ae56a338c5b2b2829
BangBang-e <youngjae7133@gmail.com>
2023-01-09 09:41:22 +0900

    2023.01.09

--- fe-sprint-react-twittler-state-props/src/App.js
+++ fe-sprint-react-twittler-state-props/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import{BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // TODO : React Router DOM을 설치 후, import 구문을 이용하여 BrowserRouter, Routes, Route 컴포넌트를 불러옵니다.
 
 import Sidebar from './Sidebar';
@@ -14,21 +14,21 @@ import './global-style.css';
 const App = (props) => {
   return (
     <BrowserRouter>
-    <div className="App">
-      <main>
-        <Sidebar />
-        <section className="features">
-          {/* TODO : 유어클래스를 참고해서, 테스트 케이스를 통과하세요.
+      <div className="App">
+        <main>
+          <Sidebar />
+          <section className="features">
+            {/* TODO : 유어클래스를 참고해서, 테스트 케이스를 통과하세요.
             TODO : React Router DOM 설치 후 BrowserRouter, Routes, Route의 주석을 해제하고 Routes, Route 컴포넌트를 적절하게 작성합니다. */}
-          {/* Route 예시: <Route path="/" element={<Tweets />}></Route> */}
-          <Routes>
-            <Route path="/" element={<Tweets />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/mypage" element={<MyPage />} />     
-          </Routes>
-        </section>
-      </main>
-    </div>
+            {/* Route 예시: <Route path="/" element={<Tweets />}></Route> */}
+            <Routes>
+              <Route path="/" element={<Tweets />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/mypage" element={<MyPage />} />
+            </Routes>
+          </section>
+        </main>
+      </div>
     </BrowserRouter>
   );
 };
